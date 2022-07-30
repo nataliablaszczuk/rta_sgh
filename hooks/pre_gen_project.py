0c957979b6278c6923de5a94c6bb60052ddc08b4
sebkaz <sebkaz@users.noreply.github.com>
2022-07-30 17:13:30 +0200

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,7 +4,7 @@ import sys
 MODULE_REGEX = r'[_a-zA-Z][_a-zA-Z0-9]+$'
 
 if __name__ == "__main__":
-    module_name = '{{ cookiecutter.repo_name }}'
+    module_name = '{{ cookiecutter.project_name }}'
     if not re.match(MODULE_REGEX, module_name):
         print('ERROR: %s is not a valid Python modeule name' % module_name)
         sys.exit(1)
